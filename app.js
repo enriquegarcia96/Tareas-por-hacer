45c7ae55bbdd97083ce446f160f20ed614213e07
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-08-29 15:38:01 -0600

    feat: 🎸 probando los modelos

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 require("colors");
 
 const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas"); //se encarga del CRUD
 
 const main = async () => {
-  console.clear();
+  
   console.log("Hola mundo");
 
   let opt = "";
 
   do {
+
     //.- Muestra el menu .-//
     opt = await inquirerMenu();
     console.log({ opt });
-
     await pausa();
+
   } while (opt !== "0");
 };
 
