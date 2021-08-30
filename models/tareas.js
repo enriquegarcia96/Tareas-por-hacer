0a4dbbdd765fc189f64f9c339b38dc12a7249293
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-08-30 12:30:12 -0600

    feat: 🎸 transformar objeto a un arreglo

--- models/tareas.js
+++ models/tareas.js
@@ -1,4 +1,4 @@
-
+const Tarea = require('./tarea');
 /**
  * _listado:
  * { 'uuid-1231231221: {id:12 , dec:asda, completadoEn:3123}  ' },
@@ -8,8 +8,33 @@ class Tareas {
 
     _listado = {};
 
+    //.- Transforma objeto a un arreglo .-//
+    get listadoArr (){
+
+        const listado = [];
+
+        //.- trae un arreglo de todas las llaves .-//
+        Object.keys(this._listado).forEach( key => {
+
+            //inserto las tareas al listado[]
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        } );
+
+        return listado
+
+    }
+
     constructor(){
         this._listado = {};
+    }
+
+
+    crearTarea( desc = '' ){
+
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+
     }
     
 }
