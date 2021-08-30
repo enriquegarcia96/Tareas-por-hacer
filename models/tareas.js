9c4eb7aa42e8c8a9167353c73634818943bee53e
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-08-30 14:42:23 -0600

    feat: 🎸 metodos de esta clase para mostrar tareas

--- models/tareas.js
+++ models/tareas.js
@@ -1,4 +1,5 @@
 const Tarea = require('./tarea');
+const inquirer = require('inquirer')
 /**
  * _listado:
  * { 'uuid-1231231221: {id:12 , dec:asda, completadoEn:3123}  ' },
@@ -22,7 +23,6 @@ class Tareas {
         } );
 
         return listado
-
     }
 
     constructor(){
@@ -30,12 +30,67 @@ class Tareas {
     }
 
 
+    cargarTareasFromArray ( tareas = [] ){
+        tareas.forEach( tarea => {
+            this._listado[tarea.id] = tarea;
+        } )
+    }
+
+
+
     crearTarea( desc = '' ){
 
         const tarea = new Tarea(desc);
-        this._listado[tarea.id] = tarea;
+        this._listado[tarea.desc] = tarea;
 
     }
+
+    listadoCompleto(){
+    
+        this.listadoArr.forEach((tarea, id) => {
+            console.log();
+            const idx = `${id + 1}`.green;
+            const {desc, completadoEn } = tarea;
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red;
+
+            console.log(`${idx} ${desc} :: ${estado}`);
+        });
+    }
+
+    listarPendientesCompletadas(completadas = true){
+
+        
+        console.log();
+        let contador = 0;
+        this.listadoArr.forEach(tarea => {
+
+            
+            const {desc, completadoEn} = tarea;
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red;
+            
+
+            if (completadas) {
+                if (completadoEn) {
+                    contador += 1;
+                    console.log(`${(contador + '.').green} ${desc} :: ${estado}`);
+                    
+                }
+            }else{
+                if (!completadoEn) {
+                    contador += 1;
+                    console.log(`${(contador + '.').red} ${desc} :: ${estado}`);
+                    
+                }
+                
+            }
+
+        })
+
+        
+
+    }
+
+    
     
 }
 
